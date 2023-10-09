b0ad5d50df224e09ed8bca79d76bdcb40f81345f
Jayaneththi <udashifin@outlook.com>
2023-10-09 10:04:47 +0300

    Initial commit

---
+++ src/PrinterApp.java
@@ -0,0 +1,33 @@
+
+public class PrinterApp {
+
+	public static void main(String[] args) {
+		System.out.println("Printer App");
+		System.out.println("This App is made by");
+		
+		String creator;
+		creator= "udashi";
+		System.out.println(creator);
+		System.out.println("This App is made by "  +  creator);		
+		int number1;
+		int number2;
+		int result;
+		int addition;
+		number1 = 5;		
+		System.out.println("The value of number1 is "+ number1);
+		number2 = 3;
+		System.out.println("The value of number2 is " + number2);
+		result= number1 * number2;
+		System.out.println(number1 + " * "+ number2+" = " + result);
+		double devision;
+		int subtraction;
+		addition= number1 + number2;
+		devision= number1 /3.3;
+		subtraction= number1 - number2;
+		System.out.println(addition);
+		System.out.println(devision);
+		 
+
+	}
+
+}
